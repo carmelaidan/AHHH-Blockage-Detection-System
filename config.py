18845323dc997ff85c7fe0db881fc565f4a5d5ce
nanashi151 <nanashikarito@gmail.com>
2026-02-20 19:19:55 +0800

    Final software pipeline: Modularized with secure config

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv() # This clever line loads the variables from .env
+# Load the database password from the hidden .env file
+load_dotenv()
 
 DB_PARAMS = {
     "dbname": "postgres",
     "user": "postgres",
-    "password": os.getenv("DB_PASSWORD"), # This is the secure way!
+    "password": os.getenv("DB_PASSWORD"),
     "host": "127.0.0.1",
     "port": "5432"
 }
+
+# The URL where our Flask API is listening
+# Make sure there are no spaces in the variable name 'API_URL'
+API_URL = "http://127.0.0.1:5000/api/water-level"
